5a1b3d167af317ab308dfd5317b4a36d8acf0740
Stanleytabot <99697986+Stanleytabot@users.noreply.github.com>
2023-01-25 09:07:54 -0600

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+terraform {
+  cloud {
+    organization = "FusionIT"
+
+    workspaces {
+      name = "gh-action-demo-stanley"
+    }
+  }
+}
